a5a5250d44ac5e0268f8a9705f6d182a49ae7ec8
zhn <frontierpro.zhn@gmail.com>
2019-04-17 19:23:32 +0800

    Admin Data Table

---
+++ sql/Admin.sql
@@ -0,0 +1,12 @@
+INSERT INTO admin VALUES
+("testroot", "abcde", "frontierpro", "13328959589", true, true, true, true, true, true, false),
+("2011020A", "admin", "Jackie", "13604660566", true, false, false, true, true, true, false),
+("2011021A", "admin", "Lincoln", "13936884747", true, false, false, true, false, false, false),
+("2011021B", "admin", "Rolster", "18945861875", true, false, false, true, true, false, false), 
+("2011022C", "admin", "rebecca", "18746643666", true, false, true, true, true, false, false), 
+("2012021A", "admin", "Michel", "13089020389", true, false, false, true, false, false, false), 
+("2013021A", "admin", "marshell", "18745357323", true, false, false, true, false, false, false),
+("2014022B", "admin", "Christina", "13345679626", true, false, false, true, true, false, false), 
+("2015010A", "admin", "whitfield", "1886858724", true, false, false, true, true, true, false),
+("2011030D", "admin", "Diffie", "18888920480", true, false, false, true, true, true, false), 
+("2010040L", "admin", "Obama", "11991111911", true, false, false, true, false, false, false);
